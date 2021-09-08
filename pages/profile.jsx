f716a6d11d0346ebcc2b86c59a356f278b182bbd
ilyes-shn <infinityilyes@gmail.com>
2021-09-08 14:21:36 +0100

    crop

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -10,11 +10,13 @@ const Login = () => {
   const router = useRouter();
   const [file, setFile] = useState(null)
   const [img, setImg] = useState('')
+  const [crop, setCrop] = useState(false)
 
 useEffect(() => {
   if (file !== null) {
     const url = URL.createObjectURL(file)
     setImg(url)
+    setCrop(true)
   }
 }, [file])
 
@@ -43,6 +45,34 @@ useEffect(() => {
             <button>Remove photo</button>
           </div>
         </div>
+        {
+          crop && (
+            <div className={styles.cropPage}>
+              <div className={styles.box}>
+                <div className={styles.cropHeader}>
+                  <h3>Crop avatar</h3>
+                  <div className={styles.close} onClick={() => setCrop(false)}>
+                  <Image height='20' width='20' src='/images/close.svg' alt='close' />
+                  </div>
+                </div>
+                <div className={styles.cropBody}>
+                  <div className={styles.cropImg}>
+                  <Image height='256' width='256' src={img || '/images/avatar.svg'} alt="avatar" />
+                  </div>
+                  <div className={styles.range}>
+                  <Image height='10' width='10' src='/images/cropIcon.svg' alt='Icon' />
+                  <input type="range" min="20" max="100" step="10" />
+                  <Image height='20' width='20' src='/images/cropIcon.svg' alt='Icon' />
+                  </div>
+                </div>
+                <div className={styles.btns}>
+                  <button onClick={() => setCrop(false)}>Cancel</button>
+                  <button onClick={() => setCrop(false)}>Save Avatar</button>
+                </div>
+              </div>
+            </div>
+          )
+        }
         <h2>Personal details</h2>
         <h4>Your personal information</h4>
         <div className={styles.form}>
